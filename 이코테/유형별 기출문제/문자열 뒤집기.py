40f6a5464c97f8ecb6fd94295c3008a11041a6df
minseunghwan <minccc42710@gmail.com>
2025-03-30 15:45:39 +0900

    +

--- 이코테/유형별 기출문제/문자열 뒤집기.py
+++ 이코테/유형별 기출문제/문자열 뒤집기.py
@@ -3,3 +3,19 @@ import sys
 input = sys.stdin.readline
 
 nums = list(map(int, input().rstrip()))
+
+cnt_0 = 0
+cnt_1 = 0
+
+for i in range(0, len(nums)):
+    if i == 0:
+        if nums[i] == 0 : cnt_0 += 1
+        elif nums[i] == 1 :  cnt_1 += 1
+    else:
+        if nums[i] != nums[i - 1]:
+            if nums[i] == 0:
+                cnt_0 += 1
+            else:
+                cnt_1 += 1
+
+print(min(cnt_0, cnt_1))
