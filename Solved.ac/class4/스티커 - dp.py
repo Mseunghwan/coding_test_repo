6419cc95a2383114f61af10b1dea82d0a8b0a2ff
Mseunghwan <minccc42710@gmail.com>
2025-05-24 15:04:41 +0900

    today commit

--- Solved.ac/class4/스티커 - dp.py
+++ Solved.ac/class4/스티커 - dp.py
@@ -14,25 +14,31 @@ for i in range(t):
     lst.append(list(map(int, input().split())))
     lst.append(list(map(int, input().split())))
 
+    if n == 1:
+        print(max(dp[0][0], dp[1][0]))
+        continue
     dp[0][0] = lst[0][0]
     dp[1][0] = lst[1][0]
     dp[0][1] = dp[1][0] + lst[0][1]
     dp[1][1] = dp[0][0] + lst[1][1]
     
     for j in range(2, n) : 
-        dp[0][j] = max(dp[1][j-2], dp[1][j-1]) + lst[0][j]
-        dp[1][j] = max(dp[0][j-2], dp[0][j-1]) + lst[1][j]
-
+        dp[0][j] = max(dp[1][j-1], dp[1][j-2]) + lst[0][j]
+        dp[1][j] = max(dp[0][j-1], dp[0][j-2]) + lst[1][j]
+        
     print(max(dp[0][n-1], dp[1][n-1]))
-    
 
     """
-    자, 점화식을 세워보면
-    dp[0][0] = lst[0][0]
-    dp[1][0] = lst[0][0]
-    dp[0][1] = max((dp[1][0] + lst[0][1]), dp[0][0])    
-    dp[1][1] = max((dp[0][0] + lst[1][1]), dp[1][0])
+    정확한 점화식:
 
-    dp[0][i] = max(dp[0][i-2], dp[1][i-1]) + lst[0][i]
-    dp[1][i] = max(dp[1][i-2], dp[0][i-1]) + lst[1][i]
-    """
+    dp[0][0] = lst[0][0]                  # 첫 번째 열, 위쪽 선택
+    dp[1][0] = lst[1][0]                  # 첫 번째 열, 아래쪽 선택
+
+    dp[0][1] = dp[1][0] + lst[0][1]       # 두 번째 열, 위쪽 선택 시 아래쪽 첫 번째 열에서 옴
+    dp[1][1] = dp[0][0] + lst[1][1]       # 두 번째 열, 아래쪽 선택 시 위쪽 첫 번째 열에서 옴
+
+    # 그 이후부터는 점화식 적용
+    for j >= 2:
+        dp[0][j] = max(dp[1][j-1], dp[1][j-2]) + lst[0][j]
+        dp[1][j] = max(dp[0][j-1], dp[0][j-2]) + lst[1][j]
+"""
